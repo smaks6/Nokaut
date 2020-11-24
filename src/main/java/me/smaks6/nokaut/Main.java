0e79b7e29ff9f53221e65e00425dd7fdf050466b
smaks6 <smaks2313@gmail.com>
2020-11-24 20:53:10 +0100

    Nokaut

--- src/main/java/me/smaks6/nokaut/Main.java
+++ src/main/java/me/smaks6/nokaut/Main.java
@@ -12,7 +12,7 @@ public class Main extends JavaPlugin implements Listener{
 	
 	private static Main instance;
 	
-    public static HashMap<String, String> gracze = new HashMap<String, String>();
+    public static HashMap<String, String> gracze = new HashMap<>();
     
 	public void onEnable() {
 		
